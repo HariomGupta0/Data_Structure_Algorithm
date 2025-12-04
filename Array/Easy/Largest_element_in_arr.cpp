1d0d9c73486dfbd00dd9d438387564c43a04c99b
Hariom_Gupta <guptahariom349@gmail.com>
2025-12-04 16:32:59 +0530

    Array questions is being solved
    Frist DSA commit

---
+++ Array/Easy/Largest_element_in_arr.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[6] ={2,4,7,8,11,3};
+    int n = sizeof(arr) / sizeof(arr[0]);
+  	int max =  arr[0];
+    for (int i=0;i<n;i++) {
+      if (arr[i] > max) {
+          max = arr[i];
+      }
+    }
+    
+    function 
+  	cout << max ;
+    return 0;
+}
