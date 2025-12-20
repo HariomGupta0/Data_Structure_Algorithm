6c191996730636c2259cb6c3014f8af717e99d85
Hariom_Gupta <guptahariom349@gmail.com>
2025-12-20 11:45:16 +0530

    String problem solved
    String commit

---
+++ String/Easy/RemovOutermostParentheses.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+
+class Solution {
+    public:
+    string RemoveParenthesis(string s) {
+        int counter = 0;
+        string ans = "";
+
+        for (int i=0; i<s.size();i++) (
+            {
+            if(s[i] == ')') counter--;
+            if(counter !=0) ans.push_back(s[i]);
+            if(s[i] == '(') counter++;
+            }
+        );
+        return ans;
+    }
+};
+
+
+int main() {
+    Solution sol;
+    string bracket = "(())(()())";
+
+    string s = sol.RemoveParenthesis(bracket);
+    cout << s;
+    return 0;
+}
